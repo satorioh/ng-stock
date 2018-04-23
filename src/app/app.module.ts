9f1cf11cae1c1a796bbc1443c403872036cdeeb0
satorioh <wangbinxp@gmail.com>
2018-04-23 17:44:54 +0800

    menu router ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ContentComponent } from './content/content.component';
-import { FooterComponent } from './footer/footer.component';
-import { MenuComponent } from './menu/menu.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { StockManageComponent } from './stock/stock-manage/stock-manage.component';
-import { StarComponent } from './star/star.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {ContentComponent} from './content/content.component';
+import {FooterComponent} from './footer/footer.component';
+import {MenuComponent} from './menu/menu.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {StockManageComponent} from './stock/stock-manage/stock-manage.component';
+import {StarComponent} from './star/star.component';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'stock', component: StockManageComponent}
+];
 
 @NgModule({
   declarations: [
@@ -21,12 +28,15 @@ import { StarComponent } from './star/star.component';
     MenuComponent,
     SidebarComponent,
     StockManageComponent,
-    StarComponent
+    StarComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-content',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
-  constructor() { }
+  pageTitle = '';
+  pageDesc = '';
+  constructor(public router:Router) {
+    
+  }
 
   ngOnInit() {
+    this.router.events.filter(event => event instanceof NavigationEnd)
+                          .subscribe((event:NavigationEnd)=>{
+                            if(event.url == '/dashboard'){
+                              this.pageTitle = '这里是首页';
+                              this.pageDesc = '天天赚钱';
+                            }else if(event.url.startsWith('/stock')){
+                              this.pageTitle = '股票信息管理';
+                              this.pageDesc = '各方信息汇总';
+                            }
+                          })
   }
 
 }
